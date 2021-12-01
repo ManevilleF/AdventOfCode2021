253e9d51e90af70f698afdaf66875c5d5e2e5f7a
Felix de Maneville <felix.maneville@gmail.com>
2021-12-01 17:52:23 +0100

    Factoring

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,9 +1,6 @@
 const FILE_PATH: &str = "input.txt";
 
-// Short Version
-
-fn get_increasing_count(values: impl Iterator<Item=u32>) -> u32
-{
+fn get_increasing_count(values: impl Iterator<Item = u32>) -> u32 {
     let (_, count) = values.fold((None, 0), |(prev, count), v| {
         let count = if v > prev.unwrap_or(v) {
             count + 1
@@ -19,14 +16,16 @@ fn main() {
     let file_content: Vec<u32> = std::fs::read_to_string(FILE_PATH)
         .unwrap()
         .split('\n')
-        .filter_map(|str| str.parse::<u32>().ok())
+        .filter_map(|str| str.parse().ok())
         .collect();
 
-    println!("Part 1: {}", get_increasing_count(file_content.iter().copied()));
+    println!(
+        "Part 1: {}",
+        get_increasing_count(file_content.iter().copied())
+    );
 
     let triple_sums = (0..file_content.len().saturating_sub(2)).map(|i| {
-        [0, 1, 2]
-            .into_iter()
+        (0..=2)
             .filter_map(|delta| file_content.get(i + delta))
             .sum::<u32>()
     });
