58b57886c417a30e2fac5cd1ea3f9d22435d20a4
Felix de Maneville <felix.maneville@gmail.com>
2021-12-03 11:50:53 +0100

    Day 3 Part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -7,20 +7,28 @@ struct BitDistribution {
 }
 
 impl BitDistribution {
-    pub const fn max_char(&self) -> char {
-        if self.count_1 > self.count_0 {
-            '1'
+    pub const fn max_bit(&self) -> Bit {
+        if self.count_1 >= self.count_0 {
+            Bit::Bit1
         } else {
-            '0'
+            Bit::Bit0
         }
     }
 
-    pub const fn min_char(&self) -> char {
+    pub fn max_char(&self) -> char {
+        self.max_bit().into()
+    }
+
+    pub const fn min_bit(&self) -> Bit {
         if self.count_1 < self.count_0 {
-            '1'
+            Bit::Bit1
         } else {
-            '0'
+            Bit::Bit0
         }
+    }
+
+    pub fn min_char(&self) -> char {
+        self.min_bit().into()
     }
 
     pub fn at(index: usize, bits: &[Vec<Bit>]) -> Self {
@@ -56,17 +64,25 @@ impl Bit {
     }
 }
 
-fn day_1(bits: &[Vec<Bit>]) {
-    let expected_len = bits.first().unwrap().len();
+impl From<Bit> for char {
+    fn from(b: Bit) -> Self {
+        match b {
+            Bit::Bit0 => '0',
+            Bit::Bit1 => '1',
+        }
+    }
+}
+
+fn day_1(bits: &[Vec<Bit>], expected_len: usize) {
     let distributions: Vec<BitDistribution> = (0..expected_len)
-        .map(|i| BitDistribution::at(i, &bits))
+        .map(|i| BitDistribution::at(i, bits))
         .collect();
     let gamma_str = BitDistribution::get_string(&distributions, BitDistribution::max_char);
     let gamma = u32::from_str_radix(&gamma_str, 2).unwrap();
     let epsilon_str = BitDistribution::get_string(&distributions, BitDistribution::min_char);
     let epsilon = u32::from_str_radix(&epsilon_str, 2).unwrap();
     println!(
-        "Gamma str: {} - val {}, Epsilon str: {} - val {}. Result: {}",
+        "Part1. Gamma str: {} - val = {}, Epsilon str: {} - val = {}. Result = {}",
         gamma_str,
         gamma,
         epsilon_str,
@@ -75,7 +91,40 @@ fn day_1(bits: &[Vec<Bit>]) {
     );
 }
 
-fn day_2(bits: &[Vec<Bit>]) {}
+fn day_2(bits: &[Vec<Bit>], max_len: usize) {
+    let (oxygen, co2) = (0..max_len).fold(
+        (bits.to_vec(), bits.to_vec()),
+        |(mut oxy_candidates, mut co2_candidate), i| {
+            if oxy_candidates.len() > 1 {
+                let oxy_bit = BitDistribution::at(i, &oxy_candidates).max_bit();
+                oxy_candidates = oxy_candidates
+                    .into_iter()
+                    .filter(|arr| *arr.get(i).unwrap() == oxy_bit)
+                    .collect();
+            }
+            if co2_candidate.len() > 1 {
+                let co2_bit = BitDistribution::at(i, &co2_candidate).min_bit();
+                co2_candidate = co2_candidate
+                    .into_iter()
+                    .filter(|arr| *arr.get(i).unwrap() == co2_bit)
+                    .collect();
+            }
+            (oxy_candidates, co2_candidate)
+        },
+    );
+    let oxygen_str: String = oxygen[0].iter().copied().map(char::from).collect();
+    let oxygen = u32::from_str_radix(&oxygen_str, 2).unwrap();
+    let co2_str: String = co2[0].iter().copied().map(char::from).collect();
+    let co2 = u32::from_str_radix(&co2_str, 2).unwrap();
+    println!(
+        "Part2. Oxygen str: {} - val = {}, CO2 str: {} - val = {}. Result = {}",
+        oxygen_str,
+        oxygen,
+        co2_str,
+        co2,
+        oxygen * co2
+    );
+}
 
 fn main() {
     let bits: Vec<Vec<Bit>> = std::fs::read_to_string(FILE_PATH)
@@ -83,6 +132,7 @@ fn main() {
         .split('\n')
         .map(|str| str.chars().filter_map(Bit::from_char).collect())
         .collect();
-    day_1(&bits);
-    day_2(&bits);
+    let expected_len = bits.first().expect("File is empty").len();
+    day_1(&bits, expected_len);
+    day_2(&bits, expected_len);
 }
