2158fb5b1cf822d5a57f897f5609c3ef76d56471
Felix de Maneville <felix.maneville@gmail.com>
2021-12-06 12:02:38 +0100

    Improved code

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,35 +1,27 @@
-use std::collections::HashMap;
-
 const FILE_PATH: &str = "input.txt";
 
-fn simulate_one_day(timers: &mut HashMap<u8, usize>) {
-    *timers = timers
-        .iter()
-        .fold(HashMap::default(), |mut map, (key, value)| {
-            let new_key = key.checked_sub(1).unwrap_or_else(|| {
-                *map.entry(8).or_insert(0) += value;
-                6
-            });
-            *map.entry(new_key).or_insert(0) += value;
-            map
-        });
+fn simulate_one_day(timers: &mut [usize; 9]) {
+    timers.rotate_left(1);
+    timers[6] += timers[8];
 }
 
 fn main() {
     let mut timers = std::fs::read_to_string(FILE_PATH)
         .unwrap()
         .split(',')
-        .filter_map(|s| s.parse::<u8>().ok())
-        .fold(HashMap::default(), |mut map, timer| {
-            *map.entry(timer).or_insert(0) += 1;
+        .filter_map(|s| s.parse::<usize>().ok())
+        .fold([0_usize; 9], |mut map, timer| {
+            if let Some(v) = map.get_mut(timer) {
+                *v += 1
+            }
             map
         });
     for day in 1..=256 {
         simulate_one_day(&mut timers);
 
         if day == 80 {
-            println!("Day {} = {}", day, timers.values().sum::<usize>());
+            println!("Day {} = {}", day, timers.iter().sum::<usize>());
         }
     }
-    println!("Day 256 = {}", timers.values().sum::<usize>());
+    println!("Day 256 = {}", timers.iter().sum::<usize>());
 }
