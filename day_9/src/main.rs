2b9faa68ef426fd548b0d3c430c5e98c57601a4f
Felix de Maneville <felix.maneville@gmail.com>
2021-12-09 10:33:07 +0100

    Day 9 Part 2

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,11 +1,12 @@
 use std::str::FromStr;
 
 const FILE_PATH: &str = "input.txt";
-
-const NEIGHBOR_COORDS: &[(usize, usize); 2] = &[
+const NEIGHBOR_COORDS: &[Coords; 2] = &[
     (1, 0), // RIGHT
     (0, 1), // TOP
 ];
+
+type Coords = (usize, usize);
 
 #[derive(Debug, Clone)]
 struct HeightMap(Vec<Vec<u8>>);
@@ -27,7 +28,7 @@ impl FromStr for HeightMap {
 }
 
 impl HeightMap {
-    fn get_neighbors_at(&self, (x, y): (usize, usize)) -> Vec<u8> {
+    fn get_neighbors_at(&self, (x, y): Coords) -> Vec<(Coords, u8)> {
         [usize::checked_add, usize::checked_sub]
             .iter()
             .flat_map(|op| {
@@ -35,32 +36,75 @@ impl HeightMap {
                     .iter()
                     .filter_map(|(dx, dy)| {
                         if let (Some(y), Some(x)) = (op(y, *dy), op(x, *dx)) {
-                            self.0.get(y).and_then(|l| l.get(x).copied())
+                            self.0
+                                .get(y)
+                                .and_then(|l| l.get(x).copied())
+                                .map(|d| ((x, y), d))
                         } else {
                             None
                         }
                     })
-                    .collect::<Vec<u8>>()
+                    .collect::<Vec<(Coords, u8)>>()
+            })
+            .collect()
+    }
+
+    fn low_points(&self) -> Vec<(Coords, u8)> {
+        self.0
+            .iter()
+            .enumerate()
+            .flat_map(|(y, line)| {
+                line.iter()
+                    .enumerate()
+                    .filter(|(x, digit)| {
+                        self.get_neighbors_at((*x, y))
+                            .iter()
+                            .all(|(_coords, n)| n > digit)
+                    })
+                    .map(|(x, digit)| ((x, y), *digit))
+                    .collect::<Vec<(Coords, u8)>>()
             })
             .collect()
     }
 
     fn risk_level(&self) -> u32 {
-        self.0
+        self.low_points()
             .iter()
-            .enumerate()
-            .map(|(y, line)| {
-                line.iter()
-                    .enumerate()
-                    .filter(|(x, digit)| self.get_neighbors_at((*x, y)).iter().all(|n| n > digit))
-                    .map(|(_x, digit)| u32::from(*digit) + 1)
-                    .sum::<u32>()
-            })
+            .map(|(_coords, digit)| u32::from(*digit) + 1)
             .sum()
+    }
+
+    fn basin_at(&self, coords: Coords, basin: &mut Vec<Coords>) {
+        if basin.contains(&coords) {
+            return;
+        }
+        basin.push(coords);
+        for (coords, _d) in self
+            .get_neighbors_at(coords)
+            .into_iter()
+            .filter(|(_coords, d)| *d < 9)
+        {
+            self.basin_at(coords, basin);
+        }
+    }
+
+    fn basin_sizes(&self) -> usize {
+        let mut sizes: Vec<usize> = self
+            .low_points()
+            .iter()
+            .map(|(coord, _d)| {
+                let mut basin = vec![];
+                self.basin_at(*coord, &mut basin);
+                basin.len()
+            })
+            .collect();
+        sizes.sort_unstable();
+        (0..3).filter_map(|_| sizes.pop()).product()
     }
 }
 
 fn main() {
     let height_map = HeightMap::from_str(&std::fs::read_to_string(FILE_PATH).unwrap()).unwrap();
     println!("Part1: risk level = {}", height_map.risk_level());
+    println!("Part2: basin sizes = {}", height_map.basin_sizes());
 }
