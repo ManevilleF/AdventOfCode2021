212e54324f9d9d7da59627131d5f1b713a4a6900
Felix de Maneville <felix.maneville@gmail.com>
2021-12-13 10:40:01 +0100

    Day 13. Part 1&2

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -1,7 +1,9 @@
+#![feature(hash_drain_filter)]
 use std::collections::HashSet;
+use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 
-const FILE_PATH: &str = "test.txt";
+const FILE_PATH: &str = "input.txt";
 
 type Coords = (u32, u32);
 
@@ -48,8 +50,62 @@ impl FromStr for Positions {
     }
 }
 
+impl Positions {
+    pub fn fold(&mut self, instruction: FoldAlong) {
+        let folded_values: HashSet<Coords> = self
+            .0
+            // WARNING: unstable, use nightly toolchain
+            .drain_filter(|(x, y)| match &instruction {
+                FoldAlong::X(x_treshold) => x_treshold < x,
+                FoldAlong::Y(y_treshold) => y_treshold < y,
+            })
+            .filter_map(|(x, y)| match &instruction {
+                FoldAlong::X(x_treshold) => x
+                    .checked_sub(*x_treshold)
+                    .and_then(|delta| x_treshold.checked_sub(delta))
+                    .map(|x| (x, y)),
+                FoldAlong::Y(y_treshold) => y
+                    .checked_sub(*y_treshold)
+                    .and_then(|delta| y_treshold.checked_sub(delta))
+                    .map(|y| (x, y)),
+            })
+            .collect();
+        for v in folded_values {
+            self.0.insert(v);
+        }
+    }
+
+    pub fn max_coords(&self) -> Coords {
+        self.0
+            .iter()
+            .fold((0, 0), |(mut x_acc, mut y_acc), (x, y)| {
+                if *x > x_acc {
+                    x_acc = *x;
+                }
+                if *y > y_acc {
+                    y_acc = *y;
+                }
+                (x_acc, y_acc)
+            })
+    }
+}
+
+impl Display for Positions {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let (x_max, y_max) = self.max_coords();
+        let buff = (0..=y_max)
+            .map(|y| {
+                (0..=x_max)
+                    .map(|x| self.0.get(&(x, y)).map(|_| '#').unwrap_or('.'))
+                    .collect::<String>()
+            })
+            .collect::<Vec<String>>();
+        write!(f, "{}", buff.join("\n"))
+    }
+}
+
 fn main() {
-    let (positions, fold_instructions) = std::fs::read_to_string(FILE_PATH)
+    let (mut positions, fold_instructions) = std::fs::read_to_string(FILE_PATH)
         .unwrap()
         .split_once("\n\n")
         .map(|(p, f)| {
@@ -61,6 +117,11 @@ fn main() {
             )
         })
         .unwrap();
-    println!("{:#?}", positions);
-    println!("{:#?}", fold_instructions);
+    for (i, fold_instruction) in fold_instructions.into_iter().enumerate() {
+        positions.fold(fold_instruction);
+        if i == 0 {
+            println!("Part 1: {} Dots visible", positions.0.len());
+        }
+    }
+    println!("Part2: \n{}", positions);
 }
