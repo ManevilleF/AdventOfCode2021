6230f6b5612c2338b811f38ff0c9a2c44426ebcc
Felix de Maneville <felix.maneville@gmail.com>
2021-12-15 15:07:17 +0100

    Part 2

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -50,17 +50,13 @@ impl Map {
                 [(1, 0), (0, 1)]
                     .iter()
                     .filter_map(|(dx, dy)| {
-                        let coord = [op(x, *dx)?, op(y, *dy)?];
-                        let cost = self.get_risk(coord)?;
-                        Some((coord, cost))
+                        let [x, y] = [op(x, *dx)?, op(y, *dy)?];
+                        let cost = self.map.get(y)?.get(x)?;
+                        Some(([x, y], *cost))
                     })
                     .collect::<Vec<(Coords, u8)>>()
             })
             .collect()
-    }
-
-    fn get_risk(&self, [x, y]: Coords) -> Option<u8> {
-        self.map.get(y)?.get(x).copied()
     }
 
     fn pop_from_stack(stack: &mut HashMap<Coords, usize>) -> Option<(Coords, usize)> {
@@ -76,9 +72,11 @@ impl Map {
         let mut stack = HashMap::new();
         stack.insert([0, 0], 0);
         let mut handled = vec![];
+        let mut results = Vec::new();
         while let Some((coord, cost)) = Self::pop_from_stack(&mut stack) {
             if coord == self.max_coords {
-                return Some(cost);
+                results.push(cost);
+                continue;
             }
             for (neighbor, new_cost) in self
                 .neighbor_coordinates(coord)
@@ -93,14 +91,56 @@ impl Map {
             }
             handled.push(coord);
         }
-        None
+        results.into_iter().min()
     }
+}
+
+fn incremented_map(map: &[Vec<u8>], delta: u8) -> Vec<Vec<u8>> {
+    map.iter().map(|vec| incremented_vec(vec, delta)).collect()
+}
+
+fn incremented_vec(vec: &[u8], delta: u8) -> Vec<u8> {
+    vec.iter()
+        .map(|v| {
+            let value = v + delta;
+            if value > 9 {
+                1
+            } else {
+                value
+            }
+        })
+        .collect()
 }
 
 fn main() {
     let map = Map::from_str(std::fs::read_to_string(FILE_PATH).unwrap().as_str()).unwrap();
-    println!(
-        "Part 1: Cheapest path = {}",
-        map.find_cheapest_path().unwrap()
-    );
+    println!("Part 1: {}", map.find_cheapest_path().unwrap());
+
+    let mut prev = map.map;
+    let new_map: Vec<Vec<u8>> = (0..5)
+        .map(|y| {
+            if y > 0 {
+                prev = incremented_map(&prev, 1);
+            }
+            prev.clone()
+        })
+        .flat_map(|map| {
+            map.iter()
+                .map(|vec| {
+                    let mut prev_vec = vec.clone();
+                    (1..5).fold(prev_vec.clone(), |mut acc, _x| {
+                        let new_vec: Vec<u8> = incremented_vec(&prev_vec, 1);
+                        acc.extend(new_vec.clone());
+                        prev_vec = new_vec;
+                        acc
+                    })
+                })
+                .collect::<Vec<Vec<u8>>>()
+        })
+        .collect();
+    let map = Map {
+        max_coords: [new_map[0].len() - 1, new_map.len() - 1],
+        map: new_map,
+    };
+    println!("Part 2: {}", map.find_cheapest_path().unwrap());
 }
