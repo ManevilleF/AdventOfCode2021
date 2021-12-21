ea43fea6a342539a64bd68eb186121df1633a3c0
Felix de Maneville <felix.maneville@gmail.com>
2021-12-21 14:34:34 +0100

    Day 21. Part 2

--- day_21/src/main.rs
+++ day_21/src/main.rs
@@ -1,7 +1,9 @@
 #![allow(clippy::cast_possible_truncation)]
+use std::collections::HashMap;
+
 const BOARD_SIZE: u32 = 10;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone)]
 struct Player {
     score: u32,
     position: u8,
@@ -41,10 +43,12 @@ impl TestDice {
 }
 
 fn main() {
-    let mut p1 = Player::new(7);
-    let mut p2 = Player::new(1);
+    let p1 = Player::new(7);
+    let p2 = Player::new(1);
     // Part 1
     {
+        let mut p1 = p1;
+        let mut p2 = p2;
         let mut dice = TestDice::default();
 
         let mut i = 0;
@@ -59,5 +63,39 @@ fn main() {
         }
         let looser = if p1.score < 1000 { &p1 } else { &p2 };
         println!("Part 1: {}", dice.count * looser.score);
+    }
+    // Part 2
+    {
+        let possible_throws: HashMap<u32, u64> =
+            vec![(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+                .into_iter()
+                .collect();
+        let (mut p1_wins, mut p2_wins) = (0_u64, 0_u64);
+        let mut stack = vec![(0, (p1, p2), 1)];
+        while let Some((i, (p1, p2), u)) = stack.pop() {
+            if p1.score >= 21 {
+                p1_wins += u;
+            } else if p2.score >= 21 {
+                p2_wins += u;
+            } else {
+                for (throw, count) in &possible_throws {
+                    if i % 2 == 0 {
+                        let mut player = p1;
+                        player.apply_dice_delta(*throw);
+                        stack.push((i + 1, (player, p2), u * count));
+                    } else {
+                        let mut player = p2;
+                        player.apply_dice_delta(*throw);
+                        stack.push((i + 1, (p1, player), u * count));
+                    }
+                }
+            }
+        }
+        println!(
+            "Part 2: P1 wins {} times and P2 {} times. Max = {}",
+            p1_wins,
+            p2_wins,
+            p1_wins.max(p2_wins)
+        );
     }
 }
