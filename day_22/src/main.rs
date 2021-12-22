d9486beb760bfe49dc69e951dec6301eec6d0838
Felix de Maneville <felix.maneville@gmail.com>
2021-12-22 09:45:41 +0100

    Day 22. Part 1

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,11 +1,12 @@
 use glam::IVec3;
 use regex::{Captures, Regex};
+use std::collections::HashSet;
 use std::str::FromStr;
 
 lazy_static::lazy_static! {
     static ref REGEX: Regex = Regex::new(r#"(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)"#).unwrap();
 }
-const FILE_PATH: &str = "test.txt";
+const FILE_PATH: &str = "input.txt";
 
 fn get_capture(captures: &Captures, index: usize) -> Result<i32, String> {
     captures
@@ -45,11 +46,52 @@ impl FromStr for BoundsOperation {
     }
 }
 
+impl BoundsOperation {
+    fn all_coords(&self) -> Vec<IVec3> {
+        (self.min.x..=self.max.x)
+            .flat_map(|x| {
+                (self.min.y..=self.max.y)
+                    .flat_map(|y| {
+                        (self.min.z..=self.max.z)
+                            .map(|z| IVec3::new(x, y, z))
+                            .collect::<Vec<IVec3>>()
+                    })
+                    .collect::<Vec<IVec3>>()
+            })
+            .collect()
+    }
+}
+
+fn fill_set(operations: &[BoundsOperation], stop_at_50: bool) -> HashSet<IVec3> {
+    let mut map = HashSet::new();
+    for operation in operations {
+        if stop_at_50
+            && (operation.min.x < -50
+                || operation.min.y < -50
+                || operation.min.z < -50
+                || operation.max.x > 50
+                || operation.max.y > 50
+                || operation.max.z > 50)
+        {
+            continue;
+        }
+        let coords = operation.all_coords();
+        if operation.operation {
+            map.extend(coords);
+        } else {
+            for coord in coords {
+                map.remove(&coord);
+            }
+        }
+    }
+    map
+}
+
 fn main() {
     let operations: Vec<BoundsOperation> = std::fs::read_to_string(FILE_PATH)
         .unwrap()
         .lines()
         .map(|l| BoundsOperation::from_str(l).unwrap())
         .collect();
-    println!("{:#?}", operations);
+    println!("Part 1: count = {}", fill_set(&operations, true).len());
 }
